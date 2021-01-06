053749e4da2fd23668d0f4f76016f8a04c4075a5
cxp504 <73679433+cxp504@users.noreply.github.com>
2021-01-06 16:19:39 +0800

    Update DCGAN.py

--- DCGAN.py
+++ DCGAN.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# from https://github.com/google/compare_gan
+# DCGAN network from https://github.com/google/compare_gan
 
 from __future__ import absolute_import
 from __future__ import division
